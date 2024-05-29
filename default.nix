87e36e275a5512e415a3da0f564bb438535523f4
samrose <samuel.rose@gmail.com>
2024-05-29 16:16:11 -0400

    init

---
+++ default.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  config,
+  dream2nix,
+  ...
+}: {
+  imports = [
+    dream2nix.modules.dream2nix.nodejs-package-lock-v3
+    dream2nix.modules.dream2nix.nodejs-granular-v3
+  ];
+
+  mkDerivation = {
+    src = config.deps.fetchFromGitHub {
+      owner = "samrose";
+      repo = "cowsay";
+      rev = "master";
+      sha256 = "sha256-hlc1Zc58csNvo8eg51K4Xk1/gRSB65S4IdDdDratyOE=";
+    };
+  };
+
+  deps = {nixpkgs, ...}: {
+    inherit
+      (nixpkgs)
+      fetchFromGitHub
+      stdenv
+      ;
+  };
+
+  nodejs-package-lock-v3 = {
+    packageLockFile = "${config.mkDerivation.src}/package-lock.json";
+  };
+
+  name = "cowsay";
+  version = "1.5.0";
+}
